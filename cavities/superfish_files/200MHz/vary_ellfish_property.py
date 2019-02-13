579ce8a9542b514288ba4ae12d40fb92bbd6a3aa
DavidPost-1 <david.posthumadeboer@physics.ox.ac.uk>
2019-02-13 17:43:22 +0000

    Move Simulation Output after each Simulation
    The previous version of this script ran superfish for all
    of the supplied values and moved the output files at the end.
    If the program ended prematurely the output files were not
    moved to the appropriate directory. The output files are now
    moved after each individual simulation.
    
    I have also added an output to the terminal to indicate how many
    simulations are left to complete.

--- cavities/superfish_files/200MHz/vary_ellfish_property.py
+++ cavities/superfish_files/200MHz/vary_ellfish_property.py
@@ -3,14 +3,22 @@ import numpy as np
 import re
 
 file_name = 'template.temp'
-new_directory = 'dome_size'
+new_directory = 'right_dome_size'
 
-property = "DOME_B"
-values = np.linspace(16, 25, 50)
+property = "RIGHT_DOME_B"
+values = np.linspace(16, 25, 10)
 
 
-for i in values:
-    print("Running Value {:.3f}".format(i))
+# Make directory structure
+subprocess.run(['mkdir', new_directory])
+subprocess.run(['mkdir', new_directory+"/results"])
+subprocess.run(['mkdir', new_directory+"/plots"])
+subprocess.run(['mkdir', new_directory+"/mesh"])
+subprocess.run(['mkdir', new_directory+"/ellfish_files"])
+subprocess.run(['mkdir', new_directory+"/other"])
+
+for n, i in enumerate(values):
+    print("Running Value {:.3f} ({}/{})".format(i, n+1, len(values)))
     with open(file_name, 'r') as input_file, open('{:.3f}.ell'.format(i), 'w') as output_file:
         for line in input_file:
             if re.match('^FILEname_prefix', line.strip()):
@@ -22,23 +30,12 @@ for i in values:
 
     subprocess.run(['wine', 'ELLFISH', '{:.3f}.ell'.format(i)])
 
-
-
-
-# Tidy Up
-subprocess.run(['mkdir', new_directory])
-subprocess.run(['mkdir', new_directory+"/results"])
-subprocess.run(['mkdir', new_directory+"/plots"])
-subprocess.run(['mkdir', new_directory+"/mesh"])
-subprocess.run(['mkdir', new_directory+"/ellfish_files"])
-subprocess.run(['mkdir', new_directory+"/other"])
-
-
-subprocess.call('mv *.SFO {}/results/'.format(new_directory), shell=True)
-subprocess.call('mv *.T35 {}/plots/'.format(new_directory), shell=True)
-subprocess.call('mv *.SEG {}/mesh/'.format(new_directory), shell=True)
-subprocess.call('mv *.AM {}/mesh/'.format(new_directory), shell=True)
-subprocess.call('mv *.ell {}/ellfish_files/'.format(new_directory), shell=True)
-subprocess.call('mv *.LOG {}/other/'.format(new_directory), shell=True)
-subprocess.call('mv *.TXT {}/other/'.format(new_directory), shell=True)
-subprocess.call('mv *.INF {}/other/'.format(new_directory), shell=True)
+    # Move files to appropriate directory
+    subprocess.call('mv *.SFO {}/results/'.format(new_directory), shell=True)
+    subprocess.call('mv *.T35 {}/plots/'.format(new_directory), shell=True)
+    subprocess.call('mv *.SEG {}/mesh/'.format(new_directory), shell=True)
+    subprocess.call('mv *.AM {}/mesh/'.format(new_directory), shell=True)
+    subprocess.call('mv *.ell {}/ellfish_files/'.format(new_directory), shell=True)
+    subprocess.call('mv *.LOG {}/other/'.format(new_directory), shell=True)
+    subprocess.call('mv *.TXT {}/other/'.format(new_directory), shell=True)
+    subprocess.call('mv *.INF {}/other/'.format(new_directory), shell=True)
